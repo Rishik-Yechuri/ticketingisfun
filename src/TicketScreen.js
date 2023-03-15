00a0b1007fce6cd5a8ca831b32d4b12520bb8619
Rishik <56663764+Rishik-Yechuri@users.noreply.github.com>
2023-03-15 00:15:58 -0500

    Removed log in
    -Removed sign up and log in(only requires email now)
    
    -Sends tickets to email instead of displaying on website

--- src/TicketScreen.js
+++ src/TicketScreen.js
@@ -9,7 +9,7 @@ import {getFunctions, httpsCallable} from "firebase/functions";
 
 //import firebase from 'firebase/app';
 
-
+//no login
 function TicketScreen(props) {
 // Import the functions you need from the SDKs you need
 // TODO: Add SDKs for Firebase products that you want to use
